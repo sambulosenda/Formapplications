9358d403ab6c19bd054c241ae26c5067c1a16439
Sambulo Senda <sambulosendas@gmail.com>
2017-12-26 16:32:37 +0000

    addeed class for soldier

---
+++ src/app/soldier.ts
@@ -0,0 +1,4 @@
+export class Soldier {
+    id: number;
+    name: string;
+}
--- src/app/soldiers/soldiers.component.ts
+++ src/app/soldiers/soldiers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Soldier} from '../soldier';
 
 @Component({
   selector: 'app-soldiers',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class SoldiersComponent implements OnInit {
- soldier = 'Windstorm';
+
+soldier: Soldier = {
+  id : 1,
+  name : 'Windstorm'
+};
 
   constructor() { }
 
